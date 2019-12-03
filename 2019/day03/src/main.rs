9ac644ae92ee36f86c45a8c28a52899aa5619268
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-03 13:52:49 +0200

    day03: fix tests

--- 2019/day03/src/main.rs
+++ 2019/day03/src/main.rs
@@ -466,7 +466,7 @@ fn simplest_stage2_example() {
 #[test]
 fn full_stage2_example1() {
 
-    let closest = stage1(&[
+    let closest = stage2(&[
         "R75,D30,R83,U83,L12,D49,R71,U7,L72",
         "U62,R66,U55,R34,D71,R55,D58,R83",
     ]);
@@ -477,7 +477,7 @@ fn full_stage2_example1() {
 #[test]
 fn full_stage2_example2() {
 
-    let closest = stage1(&[
+    let closest = stage2(&[
         "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
         "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
     ]);
