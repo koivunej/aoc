84940d3bfd268ad4b4cdee701043993cc81c1063
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-04 23:18:52 +0200

    day04.bash: fix ... broken regex which worked anyway?

--- 2019/day04.bash
+++ 2019/day04.bash
@@ -5,13 +5,13 @@ set -o pipefail
 
 echo -n 'stage1: '
 seq "$1" "$2" \
-	| egrep '^(1*2*3*4*5*6*7*8*9*)?$' \
+	| egrep '^1*2*3*4*5*6*7*8*9*$' \
 	| egrep '(.)\1' \
 	| wc -l
 
 echo -n 'stage2: '
 seq "$1" "$2" \
-	| egrep '^1*2*3*4*5*6*7*8*9*)?$' \
+	| egrep '^1*2*3*4*5*6*7*8*9*$' \
 	| egrep '(.)\1' \
 	| while read line; do \
 		grep -o . <<< "$line" \
