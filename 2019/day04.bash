7f73e1016f80a4b223317ea5ae154f39641f10f1
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-04 19:05:28 +0200

    day04.bash: handle 2nd stage as well

--- 2019/day04.bash
+++ 2019/day04.bash
@@ -5,11 +5,19 @@ set -o pipefail
 
 echo -n 'stage1: '
 seq "$1" "$2" \
-	| egrep '^(1+)?(2+)?(3+)?(4+)?(5+)?(6+)?(7+)?(8+)?(9+)?$' \
-	| egrep '(.)\1'
+	| egrep '^(1*2*3*4*5*6*7*8*9*)?$' \
+	| egrep '(.)\1' \
+	| wc -l
 
-# echo -n 'stage2: '
-# seq "$1" "$2" \
-# 	| egrep '^(1+)?(2+)?(3+)?(4+)?(5+)?(6+)?(7+)?(8+)?(9+)?$' \
-# 	| egrep '(.)\1' \
-# 	| egrep -v '(.)\1\1'
+echo -n 'stage2: '
+seq "$1" "$2" \
+	| egrep '^1*2*3*4*5*6*7*8*9*)?$' \
+	| egrep '(.)\1' \
+	| while read line; do \
+		grep -o . <<< "$line" \
+			| uniq -c \
+			| sort -nr \
+			| egrep -q '^\s+2\s' \
+			&& echo "$line"
+	done \
+	| wc -l
