063710ce79b27308303e68a32ae762d9754745cc
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-06 00:47:34 +0200

    intcode: continue splitting

--- 2019/intcode/src/lib.rs
+++ 2019/intcode/src/lib.rs
@@ -3,8 +3,10 @@ use instr::{Operation, OpCode};
 
 mod instr;
 mod error;
+mod util;
 
 pub use error::*;
+pub use util::{ParsingError, parse_program};
 
 /// Configuration for the virtual machine; default will provide the minimum required.
 /// Basically betting for restrictions on the VM operation... Not sure why.
@@ -230,44 +232,6 @@ impl<'a> Program<'a> {
             env,
         };
         p.eval()
-    }
-}
-
-#[derive(Debug)]
-pub enum ParsingError {
-    Io(std::io::Error, usize),
-    Int(std::num::ParseIntError, usize, String),
-}
-
-pub fn parse_program<R: std::io::BufRead>(mut r: R) -> Result<Vec<isize>, ParsingError> {
-    use std::str::FromStr;
-
-    let mut data = vec![];
-    let mut buffer = String::new();
-    let mut line = 0;
-
-    loop {
-        buffer.clear();
-        let bytes = r
-            .read_line(&mut buffer)
-            .map_err(|e| ParsingError::Io(e, line))?;
-
-        if bytes == 0 {
-            return Ok(data);
-        }
-
-        let parts = buffer.trim().split(',').map(isize::from_str);
-
-        for part in parts {
-            let part = match part {
-                Ok(part) => part,
-                Err(e) => return Err(ParsingError::Int(e, line, buffer)),
-            };
-
-            data.push(part);
-        }
-
-        line += 1;
     }
 }
 
---
+++ 2019/intcode/src/util.rs
@@ -0,0 +1,37 @@
+#[derive(Debug)]
+pub enum ParsingError {
+    Io(std::io::Error, usize),
+    Int(std::num::ParseIntError, usize, String),
+}
+
+pub fn parse_program<R: std::io::BufRead>(mut r: R) -> Result<Vec<isize>, ParsingError> {
+    use std::str::FromStr;
+
+    let mut data = vec![];
+    let mut buffer = String::new();
+    let mut line = 0;
+
+    loop {
+        buffer.clear();
+        let bytes = r
+            .read_line(&mut buffer)
+            .map_err(|e| ParsingError::Io(e, line))?;
+
+        if bytes == 0 {
+            return Ok(data);
+        }
+
+        let parts = buffer.trim().split(',').map(isize::from_str);
+
+        for part in parts {
+            let part = match part {
+                Ok(part) => part,
+                Err(e) => return Err(ParsingError::Int(e, line, buffer)),
+            };
+
+            data.push(part);
+        }
+
+        line += 1;
+    }
+}
