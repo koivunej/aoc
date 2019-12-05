2f44a1c45037f11ab22ce7e5826d87a4b15db6d6
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-06 00:49:14 +0200

    intcode: split environment

--- 2019/intcode/src/lib.rs
+++ 2019/intcode/src/lib.rs
@@ -4,9 +4,11 @@ use instr::{Operation, OpCode};
 mod instr;
 mod error;
 mod util;
+mod env;
 
 pub use error::*;
 pub use util::{ParsingError, parse_program};
+pub use env::Environment;
 
 /// Configuration for the virtual machine; default will provide the minimum required.
 /// Basically betting for restrictions on the VM operation... Not sure why.
@@ -47,82 +49,6 @@ trait Decoder {
     type Operation: DecodedOperation;
 
     fn decode(&self, ip: usize, value: isize) -> Result<Self::Operation, InvalidProgram>;
-}
-
-#[derive(Debug)]
-pub enum Environment {
-    NoIO,
-    Once(Option<isize>, Option<isize>),
-    Collector(Option<isize>, Vec<isize>),
-}
-
-impl std::default::Default for Environment {
-    fn default() -> Self {
-        Self::NoIO
-    }
-}
-
-impl Environment {
-    fn input(&mut self) -> Result<isize, ProgramError> {
-        match *self {
-            Environment::NoIO => Err(ProgramError::NoMoreInput),
-            Environment::Once(ref mut input, _) | Environment::Collector(ref mut input, _) => input
-                .take()
-                .ok_or(ProgramError::NoMoreInput),
-        }
-    }
-
-    fn output(&mut self, value: isize) -> Result<(), ProgramError> {
-        match *self {
-            Environment::NoIO => Err(ProgramError::CannotOutput),
-            Environment::Once(_, ref mut output) => {
-                if output.is_some() {
-                    Err(ProgramError::CannotOutput)
-                } else {
-                    *output = Some(value);
-                    Ok(())
-                }
-            }
-            Environment::Collector(_, ref mut collected) => {
-                collected.push(value);
-                Ok(())
-            }
-        }
-    }
-
-    pub fn once(input: Option<isize>) -> Self {
-        Self::Once(input, None)
-    }
-
-    pub fn collector(input: Option<isize>) -> Self {
-        Self::Collector(input, Vec::new())
-    }
-
-    pub fn unwrap_input_consumed_once(self) -> Option<isize> {
-        match self.unwrap_once() {
-            (None, x) => x,
-            (Some(unconsumed), _) => unreachable!("Input {} was not consumed", unconsumed),
-        }
-    }
-
-    pub fn unwrap_once(self) -> (Option<isize>, Option<isize>) {
-        match self {
-            Environment::Once(input, output) => (input, output),
-            x => unreachable!("Was not once: {:?}", x),
-        }
-    }
-
-    pub fn unwrap_collected(self) -> Vec<isize> {
-        match self {
-            Environment::Collector(_, collected) => collected,
-            x => unreachable!("Was not collector: {:?}", x),
-        }
-    }
-}
-
-impl IO for Environment {
-    fn input(&mut self) -> Result<isize, ProgramError> { self.input() }
-    fn output(&mut self, value: isize) -> Result<(), ProgramError> { self.output(value) }
 }
 
 pub struct Program<'a> {
