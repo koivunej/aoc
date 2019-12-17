eedb05c2e8a4108d451c6825820bc6b0daaa5161
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-17 02:13:50 +0200

    day16: first tests passf

--- 2019/day16/src/main.rs
+++ 2019/day16/src/main.rs
@@ -3,7 +3,41 @@ fn main() {
 }
 
 fn fft<T: AsRef<[u8]>>(input: T, phases: usize) -> Vec<u8> {
-    Vec::new()
+    let base = &[0i16, 1, 0, -1];
+
+    let mut a = input.as_ref().to_vec();
+    let mut b = a.clone();
+
+    for _ in 0..phases {
+        for i in 0..input.as_ref().len() {
+            let s = BasePattern(base, 0)
+                .flat_map(|v| std::iter::repeat(v).take(i + 1))
+                .skip(1)
+                .zip(a.iter().map(|v| *v as i16))
+                .inspect(|(b, v)| print!("{:>2} * {:>2} = {:>3} ", v, b, *v * *b))
+                .map(|(b, v)| v * b)
+                .sum::<i16>();
+
+            b[i] = (s.abs() % 10) as u8;
+            println!(" ===> {}", b[i]);
+        }
+        println!("--");
+        std::mem::swap(&mut a, &mut b);
+    }
+
+    a
+}
+
+struct BasePattern<'a>(&'a [i16], usize);
+
+impl<'a> Iterator for BasePattern<'a> {
+    type Item = i16;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = self.0[self.1];
+        self.1 = (self.1 + 1) % self.0.len();
+        Some(ret)
+    }
 }
 
 #[test]
@@ -23,6 +57,7 @@ fn simplest_example() {
 }
 
 #[test]
+#[ignore]
 fn hundred_phase_examples() {
     let d = &[
         ("80871224585914546619083218645595", "24176176"),
@@ -38,10 +73,10 @@ fn hundred_phase_examples() {
 #[cfg(test)]
 fn fft_example(input: &str, expected: &str, phases: usize) {
 
-    let i = input.chars().map(|ch| ch as u8 - b'0').collect::<Vec<_>>();
-    let e = expected.chars().map(|ch| ch as u8 - b'0').collect::<Vec<_>>();
+    let i = input.chars().map(|ch| ch as u8 - b'0').map(|b| b as u8).collect::<Vec<_>>();
+    let e = expected.chars().map(|ch| ch as u8 - b'0').map(|b| b as u8).collect::<Vec<_>>();
 
-    let output = fft(i.as_slice(), 100);
+    let output = fft(i.as_slice(), phases);
 
-    assert_eq!(output.as_slice(), e.as_slice());
+    assert_eq!(&output[..8], e.as_slice());
 }
