a55a1916eeb75086b7c46567b3674f1f45aa7c1c
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-19 14:56:11 +0200

    day18: add first real example, more parsing checks

--- 2019/day18/src/main.rs
+++ 2019/day18/src/main.rs
@@ -1,7 +1,7 @@
-use intcode::util::GameDisplay;
+use intcode::{util::GameDisplay, Word};
 use std::convert::TryFrom;
-use std::str::FromStr;
 use std::fmt;
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -13,6 +13,7 @@ fn steps_to_collect_all_keys(m: &Map) -> usize {
 
 struct Map {
     gd: GameDisplay<Tile>,
+    portal_at: (Word, Word),
 }
 
 impl fmt::Display for Map {
@@ -21,7 +22,7 @@ impl fmt::Display for Map {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum Tile {
     Empty,
     Wall,
@@ -72,18 +73,19 @@ impl FromStr for Map {
     type Err = InvalidTile;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let gd = s.chars()
-            .scan((0i64, 0i64), |mut acc, ch| {
-                match ch {
-                    '\n' => {
-                        acc.1 += 1;
-                        acc.0 = 0;
-                        Some((*acc, None))
-                    },
-                    _ => {
-                        acc.0 += 1;
-                        Some((*acc, Some(ch)))
-                    }
+        let (gd, portal_at) = s
+            .trim()
+            .chars()
+            .scan((0i64, 0i64), |mut acc, ch| match ch {
+                '\n' => {
+                    acc.1 += 1;
+                    acc.0 = 0;
+                    Some((*acc, None))
+                }
+                _ => {
+                    let old = *acc;
+                    acc.0 += 1;
+                    Some((old, Some(ch)))
                 }
             })
             .filter_map(|(pos, ch)| match ch {
@@ -91,13 +93,25 @@ impl FromStr for Map {
                 None => None,
             })
             .map(|(pos, ch)| (pos, Tile::try_from(ch)))
-            .fold(Ok(GameDisplay::default()), |gd, (pos, tile)| {
-                let mut gd = gd.unwrap();
-                gd.insert(&pos, tile?);
-                Ok(gd)
+            .fold(Ok((GameDisplay::default(), None)), |gd, (pos, tile)| {
+                let (mut gd, mut portal_at) = gd.unwrap();
+                let tile = tile?;
+                portal_at = match (&tile, portal_at) {
+                    (&Tile::Portal, None) => Some(pos),
+                    (&Tile::Portal, Some(old)) => {
+                        panic!("too many portals found: {:?} and {:?}", old, pos)
+                    }
+                    (_, x @ _) => x,
+                };
+
+                gd.insert(&pos, tile);
+                Ok((gd, portal_at))
             })?;
 
-        Ok(Map { gd })
+        Ok(Map {
+            gd,
+            portal_at: portal_at.expect("There must be a portal somewhere in the map"),
+        })
     }
 }
 
@@ -111,4 +125,19 @@ fn parse_first_map() {
     let m = Map::from_str(s).unwrap();
 
     assert_eq!(s.trim(), format!("{}", m).trim());
+    assert_eq!((5, 1), m.portal_at);
+
+    assert_eq!(&Tile::Wall, m.gd.get(&(0, 0)).unwrap());
+}
+
+#[test]
+fn full_first_example() {
+    let s = "\
+#########
+#b.A.@.a#
+#########";
+
+    let m = Map::from_str(s).unwrap();
+
+    assert_eq!(steps_to_collect_all_keys(&m), 8);
 }
