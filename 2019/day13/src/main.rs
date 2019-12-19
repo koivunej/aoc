026fbdb4a72491ad0c4e10775679b4e94731a478
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-19 09:12:55 +0200

    refactor gamedisplay from day13 to intcode::utils

--- 2019/day13/src/main.rs
+++ 2019/day13/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::convert::TryFrom;
 use std::fmt;
-use intcode::{util::parse_stdin_program_n_lines, Program, Environment, Word, ExecutionState, Registers};
+use intcode::{util::{parse_stdin_program_n_lines, GameDisplay}, Program, Environment, Word, ExecutionState, Registers};
 
 fn main() {
     let data = parse_stdin_program_n_lines(Some(1));
@@ -36,6 +36,12 @@ enum TileKind {
     Block,
     Paddle,
     Ball
+}
+
+impl Default for TileKind {
+    fn default() -> Self {
+        TileKind::Empty
+    }
 }
 
 impl TileKind {
@@ -74,160 +80,6 @@ impl fmt::Display for TileKind {
             Ball   => 'O',
         };
         write!(fmt, "{}", ch)
-    }
-}
-
-#[derive(Default)]
-struct GameDisplay {
-    cells: Vec<TileKind>,
-    // coordinates of the left top corner or bottom?
-    smallest_coordinates: (Word, Word),
-    width: usize,
-    height: usize,
-}
-
-impl fmt::Display for GameDisplay {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        let width = self.width();
-
-        let mut any_newline = false;
-
-        for offset in 0..self.cells.len() {
-            if offset > 0 && offset % width == 0 {
-                writeln!(fmt, "")?;
-                any_newline = true;
-            }
-            write!(fmt, "{}", self.cells.get(offset).unwrap_or(&TileKind::Empty))?;
-        }
-
-        if any_newline && false {
-            // not sure if this was a good idea after all
-            writeln!(fmt, "")?;
-        }
-
-        Ok(())
-    }
-}
-
-impl GameDisplay {
-    fn to_index(&self, p: &(Word, Word)) -> Option<usize> {
-        let (x, y) = *p;
-        let w = self.width as Word;
-        let h = self.height as Word;
-
-        let contained =
-            x >= self.smallest_coordinates.0
-            && y >= self.smallest_coordinates.1
-            && x < self.smallest_coordinates.0 + w
-            && y < self.smallest_coordinates.1 + h;
-
-        if contained {
-            let (dx, dy) = (x - self.smallest_coordinates.0, y - self.smallest_coordinates.1);
-            let offset = dy * w + dx;
-            assert!(offset >= 0, "offset shouldn't be negative: {}", offset);
-            Some(offset as usize)
-        } else {
-            None
-        }
-    }
-
-    fn width(&self) -> usize {
-        self.width
-    }
-
-    #[allow(dead_code)]
-    fn height(&self) -> usize {
-        self.height
-    }
-
-    fn insert(&mut self, p: &(Word, Word), t: TileKind) {
-        if self.cells.is_empty() {
-            self.smallest_coordinates = *p;
-            self.cells.push(t);
-            self.width = 1;
-            self.height = 1;
-            return;
-        }
-
-        loop {
-
-            // this could be Result<index, OutsideCoordinates::Before(Word, Word)> where err would be "how much outside"
-            if let Some(index) = self.to_index(p) {
-                //if self.cells[index].is_indestructible() && self.cells[index] != t {
-                //    panic!("Attempting to overwrite indestructible {:?} at {:?} with {:?}", self.cells[index], p, t);
-                //}
-                self.cells[index] = t;
-                return;
-            }
-
-            let (x, y) = *p;
-            let (mut dx, mut dy) = (x - self.smallest_coordinates.0, y - self.smallest_coordinates.1);
-
-            if dx > 0 {
-                dx -= self.width as Word;
-                dx += 1;
-                // this can become zero if we didn't need to grow there
-                assert!(dx >= 0);
-            }
-
-            if dy > 0 {
-                dy -= self.height as Word;
-                dy += 1;
-                // same as with dx and the zero
-                assert!(dy >= 0);
-            }
-
-            assert!(dx != 0 || dy != 0, "Both directions became zero for {:?} when {}x{} and {:?}", p, self.width, self.height, self.smallest_coordinates);
-
-            if dx != 0 {
-                // we need to grow columns
-                let mut next = Vec::new();
-                let next_len = (self.width + dx.abs() as usize) * self.height;
-                next.reserve(next_len);
-                std::mem::swap(&mut self.cells, &mut next);
-
-                while self.cells.len() < next_len {
-                    if dx < 0 {
-                        for _ in 0..dx.abs() {
-                            self.cells.push(TileKind::Empty);
-                        }
-                    }
-                    self.cells.extend(next.drain(..self.width));
-                    if dx > 0 {
-                        for _ in 0..dx.abs() {
-                            self.cells.push(TileKind::Empty);
-                        }
-                    }
-                }
-
-                if dx < 0 {
-                    self.smallest_coordinates.0 += dx;
-                }
-                self.width += dx.abs() as usize;
-                continue;
-            }
-
-            // need to prepend lines
-            let mut next = vec![TileKind::Empty; self.width() * dy.abs() as usize];
-            if dy < 0 {
-                next.reserve(self.cells.len());
-                std::mem::swap(&mut self.cells, &mut next);
-                // names get confusing here but for a while "next" contains our previous cells
-                // which are then moved to the end of the game board
-            }
-            self.cells.extend(next.into_iter());
-
-            if dy < 0 {
-                self.smallest_coordinates.1 += dy;
-            }
-            self.height += dy.abs() as usize;
-        }
-    }
-
-    #[allow(dead_code)]
-    fn get(&self, p: &(Word, Word)) -> Option<&TileKind> {
-        self.to_index(p)
-            .and_then(|index| self.cells.get(index))
     }
 }
 
@@ -311,63 +163,4 @@ fn stage2(data: &[Word]) -> Word {
     println!();
 
     score
-}
-
-#[test]
-fn gamedisplay_grows() {
-    let mut gd = GameDisplay::default();
-
-    gd.insert(&(1, 1), TileKind::Ball);
-    assert_eq!(gd.smallest_coordinates, (1, 1));
-    assert_eq!(format!("{}", gd).as_str(), "O");
-    assert_eq!((gd.width(), gd.height()), (1, 1));
-
-    gd.insert(&(0, 0), TileKind::Block);
-    assert_eq!(gd.smallest_coordinates, (0, 0));
-    assert_eq!((gd.width(), gd.height()), (2, 2));
-    assert_eq!(format!("{}", gd).as_str(), "X \n O");
-
-    gd.insert(&(1, 0), TileKind::Block);
-    assert_eq!((gd.width(), gd.height()), (2, 2));
-    assert_eq!(format!("{}", gd).as_str(), "XX\n O");
-
-    gd.insert(&(2, 0), TileKind::Block);
-    assert_eq!((gd.width(), gd.height()), (3, 2));
-    assert_eq!(format!("{}", gd).as_str(), "XXX\n O ");
-
-    gd.insert(&(2, 2), TileKind::Block);
-    assert_eq!((gd.width(), gd.height()), (3, 3));
-    assert_eq!(format!("{}", gd).as_str(), "XXX\n O \n  X");
-
-    gd.insert(&(-1, -1), TileKind::Block);
-    assert_eq!(gd.smallest_coordinates, (-1, -1));
-    assert_eq!((gd.width(), gd.height()), (4, 4));
-    assert_eq!(format!("{}", gd).as_str(), "X   \n XXX\n  O \n   X");
-
-    let mut contents = vec![TileKind::Empty; 16];
-    contents[0] = TileKind::Block;
-    contents[5] = TileKind::Block;
-    contents[6] = TileKind::Block;
-    contents[7] = TileKind::Block;
-    contents[10] = TileKind::Ball;
-    contents[15] = TileKind::Block;
-
-    let checks = (-1..3).into_iter()
-        .flat_map(|y| ((-1..3).into_iter().map(move |x| (x, y))))
-        .enumerate();
-
-    for (i, (x, y)) in checks {
-        println!("{} ({}, {})", i, x, y);
-        assert_eq!(contents.get(i), gd.get(&(x, y)), "Failed at index {} or ({}, {})", i, x, y);
-    }
-
-    for y in -1..4 {
-        assert_eq!(None, gd.get(&(-2, y)));
-        assert_eq!(None, gd.get(&( 3, y)));
-    }
-
-    for x in -2..4 {
-        assert_eq!(None, gd.get(&(x, -2)));
-        assert_eq!(None, gd.get(&(x,  3)));
-    }
 }
--- 2019/intcode/src/util.rs
+++ 2019/intcode/src/util.rs
@@ -83,3 +83,252 @@ pub fn with_parsed_program<V, F>(f: F) -> V
 
     f(&data)
 }
+
+mod gamedisplay {
+    use crate::Word;
+    use std::fmt;
+
+    /// Does not really belong to `intcode` but useful for maps and displays.
+    #[derive(Default)]
+    pub struct GameDisplay<T> {
+        cells: Vec<T>,
+        // coordinates of the left top corner or bottom?
+        smallest_coordinates: (Word, Word),
+        width: usize,
+        height: usize,
+    }
+
+    impl<T: fmt::Display + Default> fmt::Display for GameDisplay<T> {
+        fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+            let width = self.width();
+
+            let mut any_newline = false;
+
+            for offset in 0..self.cells.len() {
+                if offset > 0 && offset % width == 0 {
+                    writeln!(fmt, "")?;
+                    any_newline = true;
+                }
+                write!(fmt, "{}", self.cells.get(offset).unwrap_or(&T::default()))?;
+            }
+
+            if any_newline && false {
+                // not sure if this was a good idea after all
+                writeln!(fmt, "")?;
+            }
+
+            Ok(())
+        }
+    }
+
+    impl<T> GameDisplay<T> {
+        fn to_index(&self, p: &(Word, Word)) -> Option<usize> {
+            let (x, y) = *p;
+            let w = self.width as Word;
+            let h = self.height as Word;
+
+            let contained =
+                x >= self.smallest_coordinates.0
+                && y >= self.smallest_coordinates.1
+                && x < self.smallest_coordinates.0 + w
+                && y < self.smallest_coordinates.1 + h;
+
+            if contained {
+                let (dx, dy) = (x - self.smallest_coordinates.0, y - self.smallest_coordinates.1);
+                let offset = dy * w + dx;
+                assert!(offset >= 0, "offset shouldn't be negative: {}", offset);
+                Some(offset as usize)
+            } else {
+                None
+            }
+        }
+
+        fn width(&self) -> usize {
+            self.width
+        }
+
+        #[allow(dead_code)]
+        fn height(&self) -> usize {
+            self.height
+        }
+
+        #[allow(dead_code)]
+        pub fn get(&self, p: &(Word, Word)) -> Option<&T> {
+            self.to_index(p)
+                .and_then(|index| self.cells.get(index))
+        }
+    }
+
+    impl<T: Default + Clone> GameDisplay<T> {
+
+        pub fn insert(&mut self, p: &(Word, Word), t: T) {
+            if self.cells.is_empty() {
+                self.smallest_coordinates = *p;
+                self.cells.push(t);
+                self.width = 1;
+                self.height = 1;
+                return;
+            }
+
+            loop {
+
+                // this could be Result<index, OutsideCoordinates::Before(Word, Word)> where err would be "how much outside"
+                if let Some(index) = self.to_index(p) {
+                    self.cells[index] = t;
+                    return;
+                }
+
+                let (x, y) = *p;
+                let (mut dx, mut dy) = (x - self.smallest_coordinates.0, y - self.smallest_coordinates.1);
+
+                if dx > 0 {
+                    dx -= self.width as Word;
+                    dx += 1;
+                    // this can become zero if we didn't need to grow there
+                    assert!(dx >= 0);
+                }
+
+                if dy > 0 {
+                    dy -= self.height as Word;
+                    dy += 1;
+                    // same as with dx and the zero
+                    assert!(dy >= 0);
+                }
+
+                assert!(dx != 0 || dy != 0, "Both directions became zero for {:?} when {}x{} and {:?}", p, self.width, self.height, self.smallest_coordinates);
+
+                if dx != 0 {
+                    // we need to grow columns
+                    let mut next = Vec::new();
+                    let next_len = (self.width + dx.abs() as usize) * self.height;
+                    next.reserve(next_len);
+                    std::mem::swap(&mut self.cells, &mut next);
+
+                    while self.cells.len() < next_len {
+                        if dx < 0 {
+                            for _ in 0..dx.abs() {
+                                self.cells.push(T::default());
+                            }
+                        }
+                        self.cells.extend(next.drain(..self.width));
+                        if dx > 0 {
+                            for _ in 0..dx.abs() {
+                                self.cells.push(T::default());
+                            }
+                        }
+                    }
+
+                    if dx < 0 {
+                        self.smallest_coordinates.0 += dx;
+                    }
+                    self.width += dx.abs() as usize;
+                    continue;
+                }
+
+                // need to prepend lines
+                let mut next = vec![T::default(); self.width() * dy.abs() as usize];
+                if dy < 0 {
+                    next.reserve(self.cells.len());
+                    std::mem::swap(&mut self.cells, &mut next);
+                    // names get confusing here but for a while "next" contains our previous cells
+                    // which are then moved to the end of the game board
+                }
+                self.cells.extend(next.into_iter());
+
+                if dy < 0 {
+                    self.smallest_coordinates.1 += dy;
+                }
+                self.height += dy.abs() as usize;
+            }
+        }
+
+    }
+
+    #[test]
+    fn gamedisplay_grows() {
+
+        #[derive(Debug, PartialEq, Clone)]
+        enum Example {
+            Ball,
+            Block,
+            Empty,
+        }
+
+        impl Default for Example {
+            fn default() -> Example {
+                Example::Empty
+            }
+        }
+
+        impl fmt::Display for Example {
+            fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+                let ch = match *self {
+                    Example::Ball => 'O',
+                    Example::Block => 'X',
+                    Example::Empty => ' ',
+                };
+
+                write!(fmt, "{}", ch)
+            }
+        }
+
+
+        let mut gd = GameDisplay::default();
+
+        gd.insert(&(1, 1), Example::Ball);
+        assert_eq!(gd.smallest_coordinates, (1, 1));
+        assert_eq!(format!("{}", gd).as_str(), "O");
+        assert_eq!((gd.width(), gd.height()), (1, 1));
+
+        gd.insert(&(0, 0), Example::Block);
+        assert_eq!(gd.smallest_coordinates, (0, 0));
+        assert_eq!((gd.width(), gd.height()), (2, 2));
+        assert_eq!(format!("{}", gd).as_str(), "X \n O");
+
+        gd.insert(&(1, 0), Example::Block);
+        assert_eq!((gd.width(), gd.height()), (2, 2));
+        assert_eq!(format!("{}", gd).as_str(), "XX\n O");
+
+        gd.insert(&(2, 0), Example::Block);
+        assert_eq!((gd.width(), gd.height()), (3, 2));
+        assert_eq!(format!("{}", gd).as_str(), "XXX\n O ");
+
+        gd.insert(&(2, 2), Example::Block);
+        assert_eq!((gd.width(), gd.height()), (3, 3));
+        assert_eq!(format!("{}", gd).as_str(), "XXX\n O \n  X");
+
+        gd.insert(&(-1, -1), Example::Block);
+        assert_eq!(gd.smallest_coordinates, (-1, -1));
+        assert_eq!((gd.width(), gd.height()), (4, 4));
+        assert_eq!(format!("{}", gd).as_str(), "X   \n XXX\n  O \n   X");
+
+        let mut contents = vec![Example::Empty; 16];
+        contents[0] = Example::Block;
+        contents[5] = Example::Block;
+        contents[6] = Example::Block;
+        contents[7] = Example::Block;
+        contents[10] = Example::Ball;
+        contents[15] = Example::Block;
+
+        let checks = (-1..3).into_iter()
+            .flat_map(|y| ((-1..3).into_iter().map(move |x| (x, y))))
+            .enumerate();
+
+        for (i, (x, y)) in checks {
+            println!("{} ({}, {})", i, x, y);
+            assert_eq!(contents.get(i), gd.get(&(x, y)), "Failed at index {} or ({}, {})", i, x, y);
+        }
+
+        for y in -1..4 {
+            assert_eq!(None, gd.get(&(-2, y)));
+            assert_eq!(None, gd.get(&( 3, y)));
+        }
+
+        for x in -2..4 {
+            assert_eq!(None, gd.get(&(x, -2)));
+            assert_eq!(None, gd.get(&(x,  3)));
+        }
+    }
+}
+
+pub use gamedisplay::GameDisplay;
