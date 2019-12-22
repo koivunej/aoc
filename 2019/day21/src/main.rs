573b3f28483a7d502aab3f3eabfc2ed4a84cdf76
Joonas Koivunen <joonas.koivunen@gmail.com>
2019-12-22 15:09:41 +0200

    day21: boilerplate

--- 2019/day21/src/main.rs
+++ 2019/day21/src/main.rs
@@ -1,3 +1,16 @@
+use intcode::{Word, util::parse_stdin_program_n_lines};
+
 fn main() {
-    println!("Hello, world!");
+    let data = parse_stdin_program_n_lines(Some(1));
+
+    println!("stage1: {}", stage1(&data[..]));
+    println!("stage2: {}", stage2(&data[..]));
+}
+
+fn stage1(data: &[Word]) -> Word {
+    unimplemented!()
+}
+
+fn stage2(data: &[Word]) -> Word {
+    unimplemented!()
 }
