4ff40de699f8b8a924329bb3126a3975a1e6b495
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-01-16 18:09:59 +0200

    day23: part2 complete!

--- 2019/day23/src/main.rs
+++ 2019/day23/src/main.rs
@@ -1,17 +1,18 @@
 use std::sync::{Arc, Mutex};
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, VecDeque, HashSet};
 use std::sync::mpsc::{channel, Sender};
 
 use intcode::{Word, util::parse_stdin_program, Program, Registers, ExecutionState};
 
 fn main() {
-    let buffers = Arc::new(Mutex::new(HashMap::new()));
+    let (zero_tx, zero_rx) = channel();
+    let inner = Arc::new(Mutex::new(SlowBusInner { buffers: HashMap::new(), last_nat: None, idle: 0, zero_tx }));
     let (tx, rx) = channel();
     let prog = parse_stdin_program();
 
-    let join_handles = (0..50).into_iter()
+    let _join_handles = (0..50).into_iter()
         .map(|addr| {
-            let bus = SlowBus { buffers: Arc::clone(&buffers), addr, sender: Sender::clone(&tx) };
+            let bus = SlowBus { bus: Arc::clone(&inner), addr, sender: Sender::clone(&tx) };
             (bus, Nic::new(&prog, addr))
         })
         .map(|(bus, mut nic)| std::thread::spawn(move || nic.run_to_halt(bus)))
@@ -21,7 +22,20 @@ fn main() {
 
     println!("part1: {:?}", part1);
 
+    let mut first_duplicate_y_sent_to_zero = None;
+
+    let mut seen_ys = HashSet::new();
+    for y in zero_rx {
+        if !seen_ys.insert(y) {
+            first_duplicate_y_sent_to_zero = Some(y);
+            break;
+        }
+    }
+
+    println!("part2: {:?}", first_duplicate_y_sent_to_zero);
+
     assert_eq!(part1, (5471, 17714));
+    assert_eq!(first_duplicate_y_sent_to_zero, Some(10982));
 }
 
 trait Bus {
@@ -29,8 +43,48 @@ trait Bus {
     fn poll_recv(&self) -> Option<(Word, Word)>;
 }
 
+struct SlowBusInner {
+    buffers: HashMap<Word, VecDeque<(Word, Word)>>,
+    last_nat: Option<(Word, Word)>,
+    idle: u64,
+    zero_tx: Sender<Word>,
+}
+
+impl SlowBusInner {
+    fn send(&mut self, src: Word, dest: Word, x: Word, y: Word) {
+        if dest == 255 {
+            self.last_nat = Some((x, y));
+        } else {
+            let msgs = self.buffers.entry(dest).or_insert_with(VecDeque::new);
+            msgs.push_back((x, y));
+        }
+    }
+
+    fn poll_recv(&mut self, addr: &Word) -> Option<(Word, Word)> {
+        assert!(*addr >= 0);
+        let ret = self.buffers.get_mut(addr).and_then(|buffer| buffer.pop_front());
+
+        if ret.is_none() {
+            self.idle |= (1 << *addr as u64);
+        } else {
+            self.idle &= !(1 << *addr as u64);
+        }
+
+        let all_idle = self.idle == 0x3ffffffffffff;
+
+        if all_idle {
+            if let Some((x, y)) = self.last_nat.take() {
+                self.buffers.entry(0).or_insert_with(VecDeque::new).push_back((x, y));
+                self.zero_tx.send(y).unwrap();
+            }
+        }
+
+        ret
+    }
+}
+
 struct SlowBus {
-    buffers: Arc<Mutex<HashMap<Word, VecDeque<(Word, Word)>>>>,
+    bus: Arc<Mutex<SlowBusInner>>,
     addr: Word,
     sender: Sender<(Word, Word)>,
 }
@@ -39,16 +93,14 @@ impl Bus for SlowBus {
     fn send(&self, dest: Word, x: Word, y: Word) {
         if dest == 255 {
             self.sender.send((x, y)).unwrap();
-        } else {
-            let mut g = self.buffers.lock().unwrap_or_else(|e| e.into_inner());
-            let msgs = g.entry(dest).or_insert_with(VecDeque::new);
-            msgs.push_back((x, y));
         }
+        let mut g = self.bus.lock().unwrap_or_else(|e| e.into_inner());
+        g.send(self.addr, dest, x, y);
     }
 
     fn poll_recv(&self) -> Option<(Word, Word)> {
-        let mut g = self.buffers.lock().unwrap_or_else(|e| e.into_inner());
-        g.get_mut(&self.addr).and_then(|buffer| buffer.pop_front())
+        let mut g = self.bus.lock().unwrap_or_else(|e| e.into_inner());
+        g.poll_recv(&self.addr)
     }
 }
 
