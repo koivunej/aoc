0571f164affb4b7ccf8b4624fff1f5afad3a7cc9
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-02-08 14:26:04 +0200

    day01: part2 passing

--- 2018/src/bin/day01.rs
+++ 2018/src/bin/day01.rs
@@ -4,7 +4,35 @@ fn main() {
     let part1 = numbers.iter().sum::<i64>();
     println!("part1: {}", part1);
 
+    let part2 = part2(&numbers)
+        .expect("there should be one repeating in an infinite cycle");
+    println!("part2: {}", part2);
+
     assert_eq!(part1, 406);
+}
+
+fn part2(freqs: &[i64]) -> Option<i64> {
+    use std::collections::HashSet;
+
+    let calibrated = freqs.iter().cycle().scan(0, |acc, next| {
+        *acc += next;
+        Some(*acc)
+    });
+
+    let mut seen = HashSet::new();
+
+    for freq in calibrated {
+        if !seen.insert(freq) {
+            return Some(freq);
+        }
+    }
+
+    None
+}
+
+#[test]
+fn scan_works_as_expected() {
+    assert_eq!(part2(&[1i64, -2, 3, 1]), Some(2));
 }
 
 fn parse_from_stdin() -> Result<Vec<i64>, std::io::Error> {
