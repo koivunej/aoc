f9045c81012b4dc4873cbca64cf2060ca120a2dc
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-02-08 16:03:53 +0200

    day02: part2 complete

--- 2018/src/bin/day02.rs
+++ 2018/src/bin/day02.rs
@@ -4,7 +4,11 @@ fn main() {
     let part1 = part1(&ids);
     println!("part1: {}", part1);
 
+    let part2 = part2(&ids);
+    println!("part2: {}", part2);
+
     assert_eq!(part1, 4920);
+    assert_eq!(part2, "fonbwmjquwtapeyzikghtvdxl");
 }
 
 fn parse_serial_from_stdin() -> Vec<String> {
@@ -45,6 +49,44 @@ fn part1<S: AsRef<str>>(ss: &[S]) -> i64 {
     repeated_counts.iter().product()
 }
 
+fn part2<S: AsRef<str>>(ss: &[S]) -> String {
+    for y in 0..ss.len() {
+        let left = ss[y].as_ref();
+
+        for x in 0..ss.len() {
+            if x == y {
+                continue;
+            }
+
+            let right = ss[x].as_ref();
+            assert_eq!(left.len(), right.len());
+
+            let mut differences = left.chars()
+                .zip(right.chars())
+                .enumerate()
+                .filter(|&(_, (a, b))| a != b)
+                .map(|(i, _)| i);
+
+            let first = differences.next();
+
+            if let Some(i) = first {
+                if let None = differences.next() {
+                    let mut ret = String::new();
+                    ret.push_str(&left[..i]);
+                    ret.push_str(&left[i + 1..]);
+                    return ret;
+                } else {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+
+    panic!("couldn't find an id with edit distance of 1");
+}
+
 #[test]
 fn part1_example() {
     let input: Vec<&'static str> = "abcdef,bababc,abbcde,abcccd,aabcdd,abcdee,ababab"
@@ -58,4 +100,19 @@ fn part1_example() {
 fn part1_accepts_vec_of_strings() {
     let vec_of_strings = vec![String::from("a"), String::from("b")];
     part1(&vec_of_strings);
+}
+
+#[test]
+fn part2_example() {
+    let input = &[
+        "abcde",
+        "fghij",
+        "klmno",
+        "pqrst",
+        "fguij",
+        "axcye",
+        "wvxyz",
+    ];
+
+    assert_eq!(part2(&input[..]), "fgij");
 }
