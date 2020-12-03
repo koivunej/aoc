cfc8e48a3289f122ed08e0947fc987582ea81b95
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-12-03 09:43:00 +0200

    refactor day03: one loop on input

--- 2020/src/bin/day03.rs
+++ 2020/src/bin/day03.rs
@@ -2,14 +2,20 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     use std::io::BufRead;
     let stdin = std::io::stdin();
     let mut stdin = stdin.lock();
-
     let mut buf = String::new();
-
-    // true for coordinates with trees, false otherwise
-    let mut map = Vec::new();
-
     let mut width = None;
-    let mut height = 0;
+
+    let slopes = &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let mut counters = slopes
+        .iter()
+        .map(|slope| TreeCounter {
+            slope,
+            x: 0,
+            skip: 1,
+            trees: 0,
+        })
+        .collect::<Vec<_>>();
 
     loop {
         buf.clear();
@@ -26,50 +32,16 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
             width = Some(buf.len() as i64);
         }
 
-        map.extend(buf.as_bytes().iter().map(|&ch| ch == b'#'));
-        height += 1;
+        counters.iter_mut().for_each(|ctr| ctr.process(buf))
     }
 
-    let width = width.unwrap();
-    let height = height;
-
-    let slopes = [
-        Position { x: 1, y: 1 },
-        Position { x: 3, y: 1 },
-        Position { x: 5, y: 1 },
-        Position { x: 7, y: 1 },
-        Position { x: 1, y: 2 },
-    ];
-
-    let trees_hit = slopes
-        .iter()
-        .map(|slope| {
-            let mut pos = Position { x: 0, y: 0 };
-
-            let mut trees = 0;
-
-            loop {
-                assert!(pos.x >= 0);
-                if pos.y >= height {
-                    break;
-                }
-
-                let tree = map[(pos.x + width * pos.y) as usize];
-
-                if tree {
-                    trees += 1;
-                }
-
-                pos += &slope;
-                pos.wrap_around_width(width);
-            }
-
-            trees
-        })
+    let counts = counters
+        .into_iter()
+        .map(|ctr| ctr.into_count())
         .collect::<Vec<_>>();
 
-    let part_one = trees_hit[1];
-    let part_two = trees_hit.into_iter().product::<usize>();
+    let part_one = counts[1];
+    let part_two = counts.iter().product::<usize>();
 
     println!("{}", part_one);
     println!("{}", part_two);
@@ -80,20 +52,30 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     Ok(())
 }
 
-struct Position {
-    x: i64,
-    y: i64,
+struct TreeCounter {
+    slope: &'static (u16, u8),
+    x: u16,
+    skip: u8,
+    trees: usize,
 }
 
-impl std::ops::AddAssign<&Position> for Position {
-    fn add_assign(&mut self, other: &Position) {
-        self.x += other.x;
-        self.y += other.y;
+impl TreeCounter {
+    fn process(&mut self, map: &str) {
+        self.skip -= 1;
+        if self.skip > 0 {
+            return;
+        }
+        let tree = map.as_bytes()[self.x as usize] == b'#';
+
+        if tree {
+            self.trees += 1;
+        }
+
+        self.x = (self.x + self.slope.0) % map.len() as u16;
+        self.skip = self.slope.1;
     }
-}
 
-impl Position {
-    fn wrap_around_width(&mut self, width: i64) {
-        self.x %= width;
+    fn into_count(self) -> usize {
+        self.trees
     }
 }
