bb27648778302ed64f9ef12a9998ead3e3f5db9b
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-12-03 08:36:46 +0200

    day03: part two!

--- 2020/src/bin/day03.rs
+++ 2020/src/bin/day03.rs
@@ -5,11 +5,12 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
 
     let mut buf = String::new();
 
-    let mut width = None;
-    let mut pos = Position { x: 0, y: 0 };
+    // true for coordinates with trees, false otherwise
+    let mut map = Vec::new();
 
-    let mut part_one = 0;
-    let mut part_two = 0;
+    let mut width = None;
+    let mut height = 0;
+
     loop {
         buf.clear();
         let read = stdin.read_line(&mut buf)?;
@@ -25,19 +26,56 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
             width = Some(buf.len() as i64);
         }
 
-        assert!(pos.x >= 0);
-        // input is ascii
-        let tree = buf.as_bytes()[pos.x as usize] == b'#';
-
-        if tree {
-            part_one += 1;
-        }
-
-        pos += Position { x: 3, y: 1 };
-        pos.wrap_around_width(width.as_ref().copied().unwrap());
+        map.extend(buf.as_bytes().iter().map(|&ch| ch == b'#'));
+        height += 1;
     }
 
+    let width = width.unwrap();
+    let height = height;
+
+    let slopes = [
+        Position { x: 1, y: 1 },
+        Position { x: 3, y: 1 },
+        Position { x: 5, y: 1 },
+        Position { x: 7, y: 1 },
+        Position { x: 1, y: 2 },
+    ];
+
+    let trees_hit = slopes
+        .iter()
+        .map(|slope| {
+            let mut pos = Position { x: 0, y: 0 };
+
+            let mut trees = 0;
+
+            loop {
+                assert!(pos.x >= 0);
+                if pos.y >= height {
+                    break;
+                }
+
+                let tree = map[(pos.x + width * pos.y) as usize];
+
+                if tree {
+                    trees += 1;
+                }
+
+                pos += &slope;
+                pos.wrap_around_width(width);
+            }
+
+            trees
+        })
+        .collect::<Vec<_>>();
+
+    let part_one = trees_hit[1];
+    let part_two = trees_hit.into_iter().product::<usize>();
+
     println!("{}", part_one);
+    println!("{}", part_two);
+
+    assert_eq!(153, part_one);
+    assert_eq!(2_421_944_712, part_two);
 
     Ok(())
 }
@@ -47,8 +85,8 @@ struct Position {
     y: i64,
 }
 
-impl std::ops::AddAssign<Position> for Position {
-    fn add_assign(&mut self, other: Position) {
+impl std::ops::AddAssign<&Position> for Position {
+    fn add_assign(&mut self, other: &Position) {
         self.x += other.x;
         self.y += other.y;
     }
